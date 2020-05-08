d6b7c8d7baf74458b0a388e58a7bbbd85b1fb055
luniclynx <luniclynx@outlook.com>
2020-05-08 11:44:59 +0200

    Add project files.

---
+++ BugRepro/Server/Controllers/WeatherForecastController.cs
@@ -0,0 +1,32 @@
+﻿using BugRepro.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BugRepro.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<object> Get()
+        {
+            var identity = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, "Test"),
+                new Claim(ClaimTypes.Email, "test@test.test"),
+            }, "Identity.Application", ClaimTypes.Name, ClaimTypes.Role);
+
+            var principal = new ClaimsPrincipal(identity);
+            await HttpContext.SignInAsync(IdentityConstants.ApplicationScheme, principal);
+            return Ok(new object());
+        }
+    }
+}
---
+++ BugRepro/Shared/WeatherForecast.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BugRepro.Shared
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+
+    public class LoginParameters
+    {
+        //[Required]
+        public string UserName { get; set; } = default!;
+
+        //[Required]
+        public string Password { get; set; } = default!;
+
+        public bool RememberMe { get; set; }
+    }
+}
